96ec256f4757d7ab125bf2415a6370f1bc62941e
YessicaM461 <yessica10fb@hotmail.es>
2026-09-16 18:35:06 -0500

    Refactor index.js to update module paths

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { getConnection } = require('./db/connect-mongo');
+const { getConnection } = require('./connect-mongo');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -12,11 +12,11 @@ app.use(cors());
 app.use(express.json());
 
 // Endpoints
-app.use('/genero', require('./Router/genero'));
-app.use('/director', require('./Router/director'));
-app.use('/productora', require('./Router/productora'));
-app.use('/tipo', require('./Router/tipo'));
-app.use('/media', require('./Router/media'));
+app.use('/genero', require('./genero'));
+app.use('/director', require('./director'));
+app.use('/productora', require('./productora'));
+app.use('/tipo', require('./tipo'));
+app.use('/media', require('./media'));
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
